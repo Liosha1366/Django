641d79f391e4296d186faafdceabf40b2ddb7318
Hroft <hroft1366@gmail.com>
2020-10-18 14:23:24 +0300

    url

--- src/proj/urls.py
+++ src/proj/urls.py
@@ -14,11 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from hello_world.views import hello_world
 
+
+# from django.urls import path
+# from django.views.generic import RedirectView
+
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello-world/', hello_world)
+    path('hello-world/', hello_world),
+    path('', include('projects.urls'))
 ]
